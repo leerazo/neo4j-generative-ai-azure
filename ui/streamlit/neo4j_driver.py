d8f006630f4f19877e754df5fda9e56b4fbb65ad
Lee Razo <flrazo@gmail.com>
2023-09-05 14:40:10 +0200

    set aura_ds to false in streamlit Neo4j driver

--- ui/streamlit/neo4j_driver.py
+++ ui/streamlit/neo4j_driver.py
@@ -8,10 +8,13 @@ user = st.secrets["NEO4J_USER"]
 password = st.secrets["NEO4J_PASSWORD"]
 db = st.secrets["NEO4J_DB"]
 
+""" Setting aura_ds=False below for now in order to enable Neo4j EE instances as well. 
+Once Aura Pro is available in Azure we can set back to True.
+Setting to False also doesn't seem to be have any apparent negative impact on true Aura DS instances """
 gds = GraphDataScience(
     host,
     auth=(user, password),
-    aura_ds=True)
+    aura_ds=False)
 
 gds.set_database("neo4j")
 
